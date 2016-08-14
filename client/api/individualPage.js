6cd698d2662c2b8c78451bd4bb33a7b7cf82cefe
Weichen Liu <kevbtc@gmail.com>
2016-08-14 11:56:13 -0700

    individual page done

--- client/api/individualPage.js
+++ client/api/individualPage.js
@@ -1,8 +1,33 @@
 Template.individualPage.helpers({
-  authInProcess: function() {
-    return Meteor.loggingIn();
-  },
-  loggedIn: function() {
-    return !!Meteor.user(); // **
-  }
-});
+    authInProcess: function() {
+        console.log(Scores.find().fetch());
+        return Meteor.loggingIn();
+    },
+    loggedIn: function() {
+        return !!Meteor.user(); // **
+    }
+ });
+
+
+  Template.individualPage.events({
+      'click #next-btn': (event) => {
+          console.log("Next Button clicked.");
+          console.log(scores);
+
+          // save scores
+          var userId = Meteor.userId();
+          const FLAG = 1;
+          for (var i = 0; i < scores.length; i++) {
+              for (var j = 1; j < scores[i].length; j++) {
+                  Scores.insert({userId: userId, criteriaId: i, candidateId: j, score: scores[i][j], flag: FLAG});
+              }
+          }
+
+          // save Arguments
+          var argu = $('textarea#arguments').val();
+          console.log("argu: " + argu);
+          Arguments.insert({userId: userId, arguments: argu});
+
+          
+      }
+  });
