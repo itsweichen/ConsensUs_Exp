ea9c276b722eccf2e94f3c28dd54586b0897d1a9
Weichen Liu <kevbtc@gmail.com>
2016-09-06 19:22:30 -0700

    update timer method

--- client/api/timer.js
+++ client/api/timer.js
@@ -1,12 +1,17 @@
 timerStart = function(stage) {
-    Session.set("timer", Date.now());
     console.log("timer - stage: " + stage);
+    Meteor.call('timer', Meteor.userId(), stage, Date.now());
 }
 
 timerEnd = function(stage) {
-    var startTime = Session.get("timer");
+    var getTime = Timer.findOne({userId: Meteor.userId(), stage: stage}) || {time: -1};
+    var startTime = getTime.time;
     var endTime = Date.now();
     var range = (endTime - startTime)/1000;
+    if (startTime == -1) {
+        range = -1
+    }
     console.log("timer - stage: " + stage + " time: " + range);
-    Meteor.call('timer', Meteor.userId(), stage, range);
+    Timer.update({_id: Timer.findOne({userId: Meteor.userId(), stage:stage})._id}, {$set: {time: range}});
 }
+·
